114bbbf281959c6753bb128f6df62544184597ea
Joffrey Armellini <Armellini1@joffreys-macbook-pro.local>
2016-01-30 13:21:20 -0500

    Initial Commit

---
+++ iSokoBeta/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iSokoBeta
+//
+//  Created by Joffrey Armellini on 2016-01-30.
+//  Copyright © 2016 Joffrey Armellini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
