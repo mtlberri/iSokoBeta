3340fd84900aef2c4054a3b8915d11aaa6ed4d65
Joffrey Armellini <Armellini1@joffreys-macbook-pro.local>
2016-01-31 16:44:53 -0500

    Stable version with Firebase autocomplete working and building without issue!

--- iSokoBeta/RegisterViewController.swift
+++ iSokoBeta/RegisterViewController.swift
@@ -1,19 +1,19 @@
 import UIKit
 
 
-class AccountCreationViewController: UIViewController {
-
+class RegisterViewController: UIViewController {
+    
     // MARK: Outlets
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -47,15 +47,15 @@ class AccountCreationViewController: UIViewController {
         }
         
     }
-
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
